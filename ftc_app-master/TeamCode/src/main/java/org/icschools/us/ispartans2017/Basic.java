cf3484a73194badb8439fc9a4a6a2b1823177f5b
Earl-Smith <32937143+Earl-Smith@users.noreply.github.com>
2017-10-19 19:11:26 -0400

    Clean up

--- ftc_app-master/TeamCode/src/main/java/org/icschools/us/ispartans2017/Basic.java
+++ ftc_app-master/TeamCode/src/main/java/org/icschools/us/ispartans2017/Basic.java
@@ -38,19 +38,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="Basic", group="Linear Opmode")
 //@Disabled
 public class Basic extends LinearOpMode {
@@ -64,50 +51,6 @@ public class Basic extends LinearOpMode {
     @Override
     public void runOpMode() {
         RunNormal();
-        //RunEncoder();
-    }
-    private void RunEncoder(){
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
-
-        //leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //int TICKS_PER_REV = 1120;
-
-
-        // Wait for the game to start (driver presses PLAY)
-        /*waitForStart();
-        runtime.reset();
-
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            if(gamepad1.a)
-            {
-                A_ButtonFired();
-            }
-        }*/
-
-    }
-    private void A_ButtonFired(){
-        leftDrive.setTargetPosition(630);
-        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftDrive.setPower(.5);
-        while (leftDrive.isBusy())
-        {
-
-        }
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
     private void RunNormal(){
         telemetry.addData("Status", "Initialized");
